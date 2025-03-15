7a800e223dafba7a5828723e509997d297221524
VedantAnand17 <VEDANTANAND0077@gmail.com>
2025-03-15 22:25:54 +0530

    withdraw logic by call

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -19,8 +19,34 @@ contract FundMe {
         // Have a minimum amount sent
         require(msg.value.getConversionRate() >= minUSD, "Not enough ETH");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] =
-            addressToAmountFunded[msg.sender] +
-            msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        // for( startIndex, EndIndex, increment/decrement)
+        for (
+            uint256 fundersIndex = 0;
+            fundersIndex < funders.length;
+            fundersIndex++
+        ) {
+            address funder = funders[fundersIndex];
+            addressToAmountFunded[funder] = 0; // reset to zero
+        }
+        funders = new address[](0);
+
+        // address(this).balance is the eth held by the contract
+
+        //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call Failed");
     }
 }
